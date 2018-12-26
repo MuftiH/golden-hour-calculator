011f04185c1ea3da8f47a23387b874bcf276a06d
MuftiH <42257253+MuftiH@users.noreply.github.com>
2018-12-25 21:17:54 -0800

    functioning website, added localization and styling

--- bundle.js
+++ bundle.js
@@ -64,7 +64,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2e2bc7c62913bc58cd7e";
+/******/ 	var hotCurrentHash = "b49663335d90aba01a06";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
